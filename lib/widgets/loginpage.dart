d111f2ed53f1a149494a4fcab6c8b29f48f0d5ce
Atul-Keshari <atulkeshari138@gmail.com>
2024-01-13 15:08:00 +0530

    image upload and login

--- lib/widgets/loginpage.dart
+++ lib/widgets/loginpage.dart
@@ -1,30 +1,23 @@
-import 'dart:io';
-import 'package:dummylogin/widgets/loginpage.dart';
+import 'package:dummylogin/widgets/homepage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
-import 'homepage.dart';
-
-class SignUpPage extends StatefulWidget {
-  const SignUpPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<SignUpPage> createState() => _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<SignUpPage> {
+class _LoginPageState extends State<LoginPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _nameController = TextEditingController();
+
   final TextEditingController _passwordController = TextEditingController();
 
-  String imageUrl = '';
-
-  void _signUp() async {
+  void _signIn() async {
     try {
-      await _auth.createUserWithEmailAndPassword(
+      await _auth.signInWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
@@ -34,15 +27,6 @@ class _LoginPageState extends State<SignUpPage> {
       print('Error during sign-up: $e');
     }
   }
-
-  @override
-  void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
-    _nameController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,7 +45,7 @@ class _LoginPageState extends State<SignUpPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   const Text(
-                    "SignUp",
+                    "Login",
                     style: TextStyle(
                         color: Color.fromRGBO(49, 39, 79, 1),
                         fontWeight: FontWeight.bold,
@@ -94,23 +78,6 @@ class _LoginPageState extends State<SignUpPage> {
                                       color:
                                           Color.fromRGBO(196, 135, 198, .3)))),
                           child: TextField(
-                            controller: _nameController,
-                            keyboardType: TextInputType.name,
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: "UserName",
-                                hintStyle:
-                                    TextStyle(color: Colors.grey.shade700)),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: const BoxDecoration(
-                              border: Border(
-                                  bottom: BorderSide(
-                                      color:
-                                          Color.fromRGBO(196, 135, 198, .3)))),
-                          child: TextField(
                             controller: _emailController,
                             keyboardType: TextInputType.emailAddress,
                             decoration: InputDecoration(
@@ -131,35 +98,15 @@ class _LoginPageState extends State<SignUpPage> {
                                 hintStyle:
                                     TextStyle(color: Colors.grey.shade700)),
                           ),
-                        ),
+                        )
                       ],
                     ),
-                  ),
-                  Center(
-                    child: IconButton(
-                        onPressed: () async {
-                          ImagePicker imagePicker = ImagePicker();
-                          XFile? file = await imagePicker.pickImage(
-                              source: ImageSource.camera);
-                          if (file == null) return;
-                          Reference reference = FirebaseStorage.instance.ref();
-                          Reference referenceRoot = reference.child('images');
-                          Reference referenceImageUpload = referenceRoot.child(
-                              DateTime.now().millisecondsSinceEpoch.toString());
-
-                          try {
-                            await referenceImageUpload.putFile(File(file.path));
-                            imageUrl =
-                                await referenceImageUpload.getDownloadURL();
-                          } catch (e) {}
-                        },
-                        icon: Icon(Icons.camera_alt)),
                   ),
                   const SizedBox(
                     height: 30,
                   ),
                   MaterialButton(
-                    onPressed: _signUp,
+                    onPressed: _signIn,
                     color: Colors.blue,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(50),
@@ -167,7 +114,7 @@ class _LoginPageState extends State<SignUpPage> {
                     height: 50,
                     child: const Center(
                       child: Text(
-                        "SignUp",
+                        "Login",
                         style: TextStyle(color: Colors.white),
                       ),
                     ),
@@ -181,19 +128,15 @@ class _LoginPageState extends State<SignUpPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Text('or'),
+                Text('or'),
                 TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginPage()));
-                  },
-                  child: const Text(
-                    'login',
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child:const Text(
+                      'signUp',
+                      style: TextStyle(color: Colors.blue),
+                    )),
               ],
             )
           ],
